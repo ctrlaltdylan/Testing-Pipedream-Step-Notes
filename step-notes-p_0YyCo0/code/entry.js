62216ec2db680e753972b968a39b61c19d2cabe9
Dylan Pierce <me@dylanjpierce.com>
2023-10-18 11:21:30 -0700

    Delete step-notes-p_0YyCo0 directory

--- step-notes-p_0YyCo0/code/entry.js
+++
@@ -1,9 +0,0 @@
-// To use any npm package, just import it
-// import axios from "axios"
-
-export default defineComponent({
-  async run({ steps, $ }) {
-    // Reference previous step data using the steps object and return data to use it in future steps
-    return steps.trigger.event
-  },
-})
